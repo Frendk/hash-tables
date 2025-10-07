cacbf78773a2165c36a387c433231ae48f1d057e
Frendk <ayushazhamsaranov@gmail.com>
2025-10-07 12:51:00 +0800

    preparation for hash tables

--- lib/binary_search.dart
+++ lib/binary_search.dart
@@ -3,14 +3,14 @@ int? binarySearch(List<int> myList, int searchValue) {
   int highIndex = myList.length - 1;
   while (lowIndex <= highIndex) {
     int middleIndex = (lowIndex + highIndex) ~/ 2;
-    final guess = myList[middleIndex];
+    var guess = myList[middleIndex];
     if (guess == searchValue) {
       return middleIndex;
     }
     if (guess < searchValue) {
       lowIndex = middleIndex + 1;
     }
-    if (guess < searchValue) {
+    if (guess > searchValue) {
       highIndex = middleIndex - 1;
     }
   }
